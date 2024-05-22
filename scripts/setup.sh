eeb4c7f4f34cf8da08c967c2181b25362b1f92da
MaxGoetzmann <maxgoetzmann@gmail.com>
2024-05-21 22:02:23 -0500

    please work

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,12 @@
 
 source ./vars.sh
 
+# Verify
+echo ""
+echo "The current kernel version below should be 5.11.0-custom:"
+echo $(uname -r)
+echo ""
+
 # install bento
 cd $HOME/enoki/bento
 git submodule update --init --recursive
@@ -18,6 +24,12 @@ rustup install nightly-2022-08-21
 rustup override set nightly-2022-08-21
 rustup component add rust-src --toolchain nightly-2022-08-21-x86_64-unknown-linux-gnu
 cd wfq
+
+# Install rust home package 0.5.5
+cd kernel
+cargo update -p home@0.5.9 --precise 0.5.5
+cd ..
+
 make
 sudo mount -t ghost ghost /sys/fs/ghost/
 sudo insmod kernel/enoki_wfq.ko
